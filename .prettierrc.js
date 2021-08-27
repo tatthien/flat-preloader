2e79c8317eecb628884cba0014bc9ff70eeb35d2
Thien Nguyen <tatthien.contact@gmail.com>
2021-08-27 11:00:36 +0700

    Add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  trailingComma: "es5",
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  proseWrap: "never",
+};
