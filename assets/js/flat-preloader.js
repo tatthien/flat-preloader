dd6093e46dc2b88aad9e73824b0782a3cd7915de
Thien Nguyen <tatthien.contact@gmail.com>
2022-11-17 23:30:52 +0700

    fix: preloader does not disappear after back navigation

--- assets/js/flat-preloader.js
+++ assets/js/flat-preloader.js
@@ -2,6 +2,13 @@ const sleep = timeout => new Promise(resolve => setTimeout(resolve, timeout))
 const overlayEl = document.getElementById('flat-preloader-overlay')
 const showPreloaderInstantly = flatPreloader.showPreloaderInstantly === '1' ? true : false
 
+// Fix error loading screen does not disappear when clicking on back button
+window.onpageshow = function(event) {
+    if (event.persisted) {
+      window.location.reload()
+    }
+};
+
 window.addEventListener('load', async function () {
   if (!overlayEl) {
     return
@@ -21,16 +28,12 @@ window.addEventListener('load', async function () {
     document.querySelectorAll('a').forEach(link => {
       link.addEventListener('click', e => {
         let href = link.getAttribute('href')
-      
         if (isIgnored(href)) {
           return
         }
-        
         e.preventDefault()
-        
         document.body.classList.add('flat-preloader-active')
         overlayEl.classList.remove('hide')
-        
         window.location.href = href
       })
     })
@@ -41,15 +44,12 @@ function isIgnored(url) {
   if (!url.includes(flatPreloader.host)) {
     return true
   }
-
   let ignore = false
-
   flatPreloader.ignores.forEach(regex => {
     if (new RegExp(regex).test(url)) {
       ignore = true
       return
     }
   })
-
   return ignore
 }
