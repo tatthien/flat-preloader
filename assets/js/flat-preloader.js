2dc61bb3ae00c8dc408d2dcc5f6e171236b33fe2
Thien Nguyen <tatthien.contact@gmail.com>
2022-11-21 21:46:41 +0700

    chore: move everything to function scope

--- assets/js/flat-preloader.js
+++ assets/js/flat-preloader.js
@@ -1,55 +1,59 @@
-const sleep = timeout => new Promise(resolve => setTimeout(resolve, timeout))
-const overlayEl = document.getElementById('flat-preloader-overlay')
-const showPreloaderInstantly = flatPreloader.showPreloaderInstantly === '1' ? true : false
+flatPreloaderInit()
 
-// Fix error loading screen does not disappear when clicking on back button
-window.onpageshow = function(event) {
+function flatPreloaderInit() {
+  const sleep = timeout => new Promise(resolve => setTimeout(resolve, timeout))
+  const overlayEl = document.getElementById('flat-preloader-overlay')
+  const showPreloaderInstantly = flatPreloader.showPreloaderInstantly === '1' ? true : false
+
+  // Fix error loading screen does not disappear when clicking on back button
+  window.onpageshow = function(event) {
     if (event.persisted) {
       window.location.reload()
     }
-};
+  };
 
-window.addEventListener('load', async function () {
-  if (!overlayEl) {
-    return
-  }
-
-  const delayTime = flatPreloader.delayTime
-
-  await sleep(delayTime)
-
-  document.body.classList.remove('flat-preloader-active')
-  overlayEl.classList.add('hide')
-
-  await sleep(300)
-  
-  // Show the preloader immediately
-  if (showPreloaderInstantly) {
-    document.querySelectorAll('a').forEach(link => {
-      link.addEventListener('click', e => {
-        let href = link.getAttribute('href')
-        if (isIgnored(href)) {
-          return
-        }
-        e.preventDefault()
-        document.body.classList.add('flat-preloader-active')
-        overlayEl.classList.remove('hide')
-        window.location.href = href
-      })
-    })
-  }
-})
-
-function isIgnored(url) {
-  if (!url.includes(flatPreloader.host)) {
-    return true
-  }
-  let ignore = false
-  flatPreloader.ignores.forEach(regex => {
-    if (new RegExp(regex).test(url)) {
-      ignore = true
+  window.addEventListener('load', async function () {
+    if (!overlayEl) {
       return
     }
+
+    const delayTime = flatPreloader.delayTime
+
+    await sleep(delayTime)
+
+    document.body.classList.remove('flat-preloader-active')
+    overlayEl.classList.add('hide')
+
+    await sleep(300)
+
+    // Show the preloader immediately
+    if (showPreloaderInstantly) {
+      document.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', e => {
+          let href = link.getAttribute('href')
+          if (isIgnored(href)) {
+            return
+          }
+          e.preventDefault()
+          document.body.classList.add('flat-preloader-active')
+          overlayEl.classList.remove('hide')
+          window.location.href = href
+        })
+      })
+    }
   })
-  return ignore
+
+  function isIgnored(url) {
+    if (!url.includes(flatPreloader.host)) {
+      return true
+    }
+    let ignore = false
+    flatPreloader.ignores.forEach(regex => {
+      if (new RegExp(regex).test(url)) {
+        ignore = true
+        return
+      }
+    })
+    return ignore
+  }
 }
